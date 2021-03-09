b7993f000c780378816be1dc38a4413f7d98882b
Viktor <viktor.johansson96@gmail.com>
2021-03-09 11:20:02 +0100

    small changes

--- src/components/Encounters.vue
+++ src/components/Encounters.vue
@@ -1,17 +1,24 @@
 <template>
 	<h1>Encounters</h1>
-	<input type="text" v-model="monsterInput"/>
+	<div v-for="encounterChar in encounterChars" :key="encounterChar.index">
+		{{ encounterChars }}
+    <button @click="removeCharFromEncounter(char)">Remove</button>
+	</div>
+	<input type="text" v-model="monsterInput" />
 	<button>Add</button>
-  <!-- <div v-for="monster in monsters" :key="monster.index">{{ monster }}</div> -->
-  <div v-for="monster in filteredMonsters" :key="monster.index">{{ monster }}</div>
+	<!-- <div v-for="monster in monsters" :key="monster.index">{{ monster }}</div> -->
+	<div v-for="monster in filteredMonsters" :key="monster.index">
+		{{ monster }} <button @click="addMonster(monster)">Add</button>
+	</div>
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-        monsterInput: '',
-				monsters: []
+				monsterInput: '',
+				monsters: [],
+				encounterChars: []
 			}
 		},
 		methods: {
@@ -19,28 +26,34 @@
 				await fetch('https://www.dnd5eapi.co/api/monsters')
 					.then((res) => res.json())
 					.then((res) => {
-            res.results.forEach(monstersArr => {
-              this.monsters = this.monsters.concat(monstersArr)
-            })
-            })
+						res.results.forEach((monstersArr) => {
+							this.monsters = this.monsters.concat(monstersArr)
+						})
+					})
+			},
+			addMonster(monster) {
+				this.encounterChars.push(monster)
+			},
+      removeCharFromEncounter(char) {
+        
+      }
+		},
+		computed: {
+			filteredMonsters() {
+				return this.monsters.filter((monster) => {
+					if (!this.monsterInput) {
+						return false
+					}
+					if (monster.index.includes(this.monsterInput)) {
+						return true
+					} else {
+						return false
+					}
+				})
 			}
 		},
-    computed: {
-      filteredMonsters() {
-        return this.monsters.filter(monster => {
-          if (!this.monsterInput) {
-            return false
-          }
-          if (monster.index.includes(this.monsterInput)) {
-            return true
-          } else {
-            return false
-          }
-        })
-      }
-    },
 		created() {
-      this.fetchAllMonsters()
-    }
+			this.fetchAllMonsters()
+		}
 	}
 </script>
